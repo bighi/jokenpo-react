6e720fffd98b355676c5444cedfa1c788127dcdd
Cecilia Lourenco <ceciliamblourenco@gmail.com>
2022-07-04 15:33:50 -0300

    feat: component HandOptions

--- components/HandOptions.js
+++ components/HandOptions.js
@@ -1,26 +1,38 @@
-import { useState } from "react"
-import HandButton from "./HandButton"
+import { useState } from "react";
+import HandButton from "./HandButton";
 
 function HandOptions() {
-  const [selectedHand, setSelectedHand] = useState()
+  const [selectedHand, setSelectedHand] = useState();
 
-  console.log("Mão selecionada é", selectedHand)
+  console.log("Mão selecionada é", selectedHand);
 
   const isActive = (type) => {
     if (selectedHand == type) {
-      return true
+      return true;
     } else {
-      return false
+      return false;
     }
-  }
+  };
 
   return (
-    <div className="w-100 d-flex justify-content-between">
-      <HandButton type="rock" active={isActive("rock")} onClick={() => setSelectedHand('rock')} />
-      <HandButton type="paper" active={isActive("paper")} onClick={() => setSelectedHand('paper')} />
-      <HandButton type="scissors" active={isActive("scissors")} onClick={() => setSelectedHand('scissors')} />
+    <div className="w-100 rounded-box mt-5">
+      <HandButton
+        type="rock"
+        active={isActive("rock")}
+        onClick={() => setSelectedHand("rock")}
+      />
+      <HandButton
+        type="paper"
+        active={isActive("paper")}
+        onClick={() => setSelectedHand("paper")}
+      />
+      <HandButton
+        type="scissors"
+        active={isActive("scissors")}
+        onClick={() => setSelectedHand("scissors")}
+      />
     </div>
-  )
+  );
 }
 
 export default HandOptions;
